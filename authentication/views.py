3eed78c2443b72a5473cf1d5b8af5ad544c07a9b
Eric Chen <echen2015@gmail.com>
2017-03-09 15:44:11 -0500

    added sending text message to verify phone on account creation. Added a post APIView for the user to verify the code, but it has not been tested yet and I am not sure it works

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,8 @@ import json
 
 from django.shortcuts import render
 
+from authy.api import AuthyApiClient
+
 from overrides import overrides
 
 from rest_framework import permissions, viewsets, status
@@ -17,9 +19,14 @@ from authentication.serializers import AccountSerializer
 from authentication.jwt_authentication import JSONWebTokenAuthenticationCookie
 
 from django.contrib.auth import authenticate
+
 from django.http import HttpResponseRedirect
 
 from django.views.generic.base import TemplateView
+
+from twilio_helper import twilio
+
+
 
 # Create your views here.
 def setCookie(account, response):
@@ -28,6 +35,67 @@ def setCookie(account, response):
     payload = jwt_payload_handler(account)
     token = jwt_encode_handler(payload)
     response.set_cookie('jwt', token, httponly=True)
+
+class VerifyPhone(APIView):
+    serializer_class = AccountSerializer
+    authentication_classes = []
+
+    def post(self, request, format=None):
+        data = request.data
+
+        email = data.get('email', None)
+        phone = data.get('phone', None)
+        verification_code = data.get('verification_code', None)
+
+        valid_input = True
+        message = ''
+
+        # Make sure all required input has been provided
+        if email is None:
+            valid_input = False
+            message = 'email missing'
+        if phone is None:
+            valid_input = False
+            'phone missing'
+        if verification_code is None:
+            valid_input = False
+            'verification_code missing'
+
+        if not valid_input:
+            return Response({
+                'status': 'Unauthorized',
+                'message': "Missing input: %s" % message
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # Verify  Email exists
+        try:
+            account = Account.objects.get(email=email)
+        except Account.DoesNotExist:
+            valid_input = False
+            return Response({
+                'status': 'Unauthorized',
+                'message': "Email does not exist"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        # Make sure Email and Phone match in the account
+        user_phone = account.phone.raw_input
+        if user_phone != phone:
+            valid_input = False
+            message = "Email and phone do not match"
+
+        # Make sure code matches
+        phone_validated = account.validate_phone_auth_code(verification_code)
+
+        if phone_validated:
+            return Response({
+                'status': "OK",
+                "message": "Phone number has been verified"
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': "Phone number code is incorrect"
+            }, status=status.HTTP_400_BAD_REQUEST)
 
 class AuthLogout(APIView):
     serializer_class = AccountSerializer
@@ -85,10 +153,13 @@ class AccountViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             # Creates the account
             account = serializer.save()
+            data = serializer.data
             response = Response(serializer.data, status=status.HTTP_201_CREATED)
 
+            twilio.send_sms_auth(account)
             # Set JWT for authentication
             setCookie(account, response)
+
 
             return response
 
---
+++ twilio_helper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TwilioHelperConfig(AppConfig):
+    name = 'twilio_helper'
